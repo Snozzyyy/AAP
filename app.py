34ae1b1a05e6d2d17729de3f8be91bd60069b114
Snozzy <choks@MacBook-Pro-64.local>
2026-07-16 10:26:46 +0800

    AAP base code

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from database import init_db
+from pages_auth import show_login, show_signup
+from pages_dashboards import show_patient_dashboard, show_doctor_dashboard
+from pages_admin import show_admin_dashboard
+from pages_features import show_feature_page, FEATURE_ROUTES
+
+st.set_page_config(
+    page_title="Healthify",
+    page_icon="",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+init_db()
+
+if "page" not in st.session_state:
+    st.session_state.page = "login"
+if "user" not in st.session_state:
+    st.session_state.user = None
+
+protected_pages = {
+    "patient_dashboard", "doctor_dashboard", "admin_dashboard"
+} | set(FEATURE_ROUTES.keys())
+
+if st.session_state.user is None and st.session_state.page in protected_pages:
+    st.session_state.page = "login"
+
+page = st.session_state.page
+
+if page == "login":
+    show_login()
+elif page == "signup":
+    show_signup()
+elif page == "patient_dashboard":
+    show_patient_dashboard()
+elif page == "doctor_dashboard":
+    show_doctor_dashboard()
+elif page == "admin_dashboard":
+    show_admin_dashboard()
+elif page in FEATURE_ROUTES:
+    show_feature_page(page)
+else:
+    st.session_state.page = "login"
+    st.rerun()
---
+++ pages_features.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from styles import coming_soon_page
+
+
+FEATURE_ROUTES = {
+    "feature_ai_symptom_patient": ("AI Symptom Diagnoser", "patient_dashboard"),
+    "feature_book_appointment": ("Book an Appointment", "patient_dashboard"),
+    "feature_appointment_notes": ("Create Appointment Notes", "doctor_dashboard"),
+    "feature_billing_assistant": ("Billing Assistant", "doctor_dashboard"),
+}
+
+
+def show_feature_page(page_key: str):
+    if page_key not in FEATURE_ROUTES:
+        st.error("Page not found.")
+        if st.button("Go to Login"):
+            st.session_state.page = "login"
+            st.rerun()
+        return
+
+    feature_name, back_page = FEATURE_ROUTES[page_key]
+    coming_soon_page(feature_name, back_page)
